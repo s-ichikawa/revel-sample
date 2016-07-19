a4b9e70e5b531b01fa7142971bbfed433146ff43
s-ichikawa <ichikawa.shingo.0829@gmail.com>
2016-07-20 08:26:35 +0900

    form作った

--- app/controllers/sample.go
+++ app/controllers/sample.go
@@ -12,5 +12,13 @@ func (c Sample) Index() revel.Result {
 }
 
 func (c Sample) Hello(myName string) revel.Result {
-	return c.Render(myName)
+    c.Validation.Required(myName).Message("Your Name is required.")
+    c.Validation.MinSize(myName, 3).Message("Your Name is not long enough.")
+
+    if (c.Validation.HasErrors()) {
+        c.Validation.Keep()
+        c.FlashParams()
+        return c.Redirect(Sample.Index)
+    }
+    return c.Render(myName)
 }
